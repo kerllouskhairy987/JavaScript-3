3caed85ce708185d52d0699fea6f047aef80c55e
kerllouskhairy987 <kyrlskhyry80@gmail.com>
2023-07-06 21:16:36 +0300

    project

---
+++ js/script.js
@@ -0,0 +1,337 @@
+
+let Website = document.querySelector(".website")
+let Website4444 = document.querySelector(".website4444")
+let Website2 = document.querySelector(".website2")
+let Website3333 = document.querySelector(".website3333")
+
+let Divsp1 = document.querySelector(".divsp1")
+let Divsp4444 = document.querySelector(".divsp4444")
+let Divsp3333 = document.querySelector(".divsp3333")
+let Divsp22 = document.querySelector(".divsp22")
+
+let Divsps2 = document.querySelector(".divsps2")
+let Divsps24444 = document.querySelector(".divsps24444")
+let Divsps23333 = document.querySelector(".divsps23333")
+let Divsps22 = document.querySelector(".divsps22")
+
+let LebelOne = document.querySelector(".lebelOne")
+let LebelOne4444 = document.querySelector(".lebelOne4444")
+let LebelOne3333 = document.querySelector(".lebelOne3333")
+let Lebel22 = document.querySelector(".label22")
+
+let IconDeletOne = document.querySelector(".iconDeletOne")
+let IconDeletOne4444 = document.querySelector(".iconDeletOne4444")
+let IconDeletOne3333 = document.querySelector(".iconDeletOne3333")
+let IconDelet22 = document.querySelector(".iconDelet22")
+
+let Div1 = document.querySelector(".div1")
+let Div22 = document.querySelector(".div22")
+let Div3333 = document.querySelector(".div3333")
+let Div4444 = document.querySelector(".div4444")
+
+let Cicleone = document.querySelector(".cicleone")
+let Cicle4444 = document.querySelector(".cicle4444")
+let Cicle3333 = document.querySelector(".cicle3333")
+let Cicle22 = document.querySelector(".cicle22")
+
+let PartOnep11 = document.querySelector(".partOnep11")
+let PartOnep4444 = document.querySelector(".partOnep4444")
+let PartOnep3333 = document.querySelector(".partOnep3333")
+let PartOnep2222 = document.querySelector(".partOnep2222")
+
+let PartOnep21 = document.querySelector(".partOnep21")
+let PartOnep44444 = document.querySelector(".partOnep44444")
+let PartOnep33333 = document.querySelector(".partOnep33333")
+let PartOnep2122 = document.querySelector(".partOnep2122")
+
+let accept1 = document.querySelector(".Accept1")
+let Closed1 = document.querySelector(".closed")
+let reject1 = document.querySelector(".Reject1")
+
+let requestOneFirst = document.querySelector(".requestOneFirst")
+let requestOneSecond = document.querySelector(".requestOneSecond")
+let requestOnethird = document.querySelector(".requestOnethird")
+
+
+let requestTwoFirst = document.querySelector(".requestTwoFirst")
+let RequestTwoSecond = document.querySelector(".requestTwoSecond")
+let requestTwoThird = document.querySelector(".requestTwoThird")
+
+let requestThreeFirst = document.querySelector(".requestThreeFirst")
+let requestThreeSecond = document.querySelector(".requestThreeSecond")
+let requestThreeThird = document.querySelector(".requestThreeThird")
+
+let RequestFourFirst = document.querySelector(".RequestFourFirst")
+let RequestFourSecond = document.querySelector(".RequestFourSecond")
+let RequestFourThird = document.querySelector(".RequestFourThird")
+
+let Cicletwo = document.querySelector(".cicletwo")
+let Cicletwo4444 = document.querySelector(".cicletwo4444")
+let Cicletwo3333 = document.querySelector(".cicletwo3333")
+let Cicletwo2222 = document.querySelector(".cicletwo2222")
+
+let PartOnep12 = document.querySelector(".partOnep12")
+let PartOnep124444 = document.querySelector(".partOnep124444")
+let PartOnep123333 = document.querySelector(".partOnep123333")
+let PartOnep122222 = document.querySelector(".partOnep122222")
+
+let PartOnep22 = document.querySelector(".partOnep22")
+let PartOnep224444 = document.querySelector(".partOnep224444")
+let PartOnep223333 = document.querySelector(".partOnep223333")
+let PartOnep222222 = document.querySelector(".partOnep222222")
+
+let accept2 = document.querySelector(".Accept2")
+let Closed2 = document.querySelector(".closed2")
+let reject2 = document.querySelector(".Reject2")
+let Ciclethree = document.querySelector(".ciclethree")
+let Ciclethree4444 = document.querySelector(".ciclethree4444")
+let Ciclethree3333 = document.querySelector(".ciclethree3333")
+let Ciclethree2222 = document.querySelector(".ciclethree2222")
+
+let PartOnep13 = document.querySelector(".partOnep13")
+let PartOnep134444 = document.querySelector(".partOnep134444")
+let PartOnep133333 = document.querySelector(".partOnep133333")
+let PartOnep132222 = document.querySelector(".partOnep132222")
+
+let PartOnep23 = document.querySelector(".partOnep23")
+let PartOnep234444 = document.querySelector(".partOnep234444")
+let PartOnep233333 = document.querySelector(".partOnep233333")
+let PartOnep232222 = document.querySelector(".partOnep232222")
+
+let accept3 = document.querySelector(".Accept3")
+let Closed3 = document.querySelector(".closed3")
+
+let reject3 = document.querySelector(".Reject3")
+
+let arr1 = [
+    {"website" :"website UIdesign implementation"},
+    {"divsp1" :"2022/7/31"},
+    {"divsps2" :"published"},
+    {"lebelOne" :"3 Request"},
+    {"partOnep11" :"Balqees Hamdi"},
+    {"partOnep21" :"Front end developer"},
+    {"Accept1" :"Accept"},
+    {"Reject1" :"Reject"},
+    {"partOnep12" :"Balqees Hamdi"},
+    {"partOnep22" :"Front end developer"},
+    {"Accept2" :"Accept"},
+    {"Reject2" :"Reject"},
+    {"partOnep13" :"Balqees Hamdi"},
+    {"partOnep23" :"Front end developer"},
+    {"Accept3" :"Accept"},
+    {"Reject3" :"Reject"},
+    {"divsps2" :"Closed"},
+    {"divsps2" :"In progress"},
+    {"divsps2" :"Rejected"},
+    {"closed" :"closed"},
+]
+
+
+window.onload = function x(){
+    Website.innerHTML = arr1[0].website ;
+    Website4444.innerHTML = arr1[0].website ;
+    Website2.innerHTML = arr1[0].website ;
+    Website3333.innerHTML = arr1[0].website ;
+    Divsp1.innerHTML = arr1[1].divsp1 ;
+    Divsp4444.innerHTML = arr1[1].divsp1 ;
+    Divsp3333.innerHTML = arr1[1].divsp1 ;
+    Divsp22.innerHTML = arr1[1].divsp1 ;
+    Divsps2.innerHTML = arr1[2].divsps2 ;
+    
+    
+    
+    LebelOne.innerHTML = arr1[3].lebelOne ;
+    LebelOne.style.backgroundColor = "#FED049";
+    LebelOne.style.borderRadius = "20px";
+    LebelOne.style.color = "#FAFAFA";
+    LebelOne4444.innerHTML = arr1[3].lebelOne ;
+    LebelOne4444.style.backgroundColor = "#FED049";
+    LebelOne4444.style.borderRadius = "20px";
+    LebelOne4444.style.color = "#FAFAFA";
+    LebelOne3333.innerHTML = arr1[3].lebelOne ;
+    LebelOne3333.style.backgroundColor = "#FED049";
+    LebelOne3333.style.borderRadius = "20px";
+    LebelOne3333.style.color = "#FAFAFA";
+    Lebel22.innerHTML = arr1[3].lebelOne ;
+    Lebel22.style.backgroundColor = "#FED049";
+    Lebel22.style.borderRadius = "20px";
+    Lebel22.style.color = "#FAFAFA";
+    IconDeletOne.innerHTML = `<i class="fa-solid fa-trash-can " ></i>` ;
+    IconDeletOne4444.innerHTML = `<i class="fa-solid fa-trash-can " ></i>` ;
+    IconDeletOne3333.innerHTML = `<i class="fa-solid fa-trash-can " ></i>` ;
+    IconDelet22.innerHTML = `<i class="fa-solid fa-trash-can " ></i>` ;
+    Div1.style.backgroundColor = "rgba(177, 242, 253, 0.158)" ;
+    Div4444.style.backgroundColor = "rgba(177, 242, 253, 0.158)" ;
+    Div3333.style.backgroundColor = "rgba(177, 242, 253, 0.158)" ;
+    Div22.style.backgroundColor = "rgba(177, 242, 253, 0.158)" ;
+    Div22.style.marginTop = "80px" ;
+    Cicleone.innerHTML = `<i class="fa-solid fa-circle"></i>` ;
+    Cicle4444.innerHTML = `<i class="fa-solid fa-circle"></i>` ;
+    Cicle3333.innerHTML = `<i class="fa-solid fa-circle"></i>` ;
+    Cicle22.innerHTML = `<i class="fa-solid fa-circle"></i>` ;
+    PartOnep11.innerHTML = arr1[4].partOnep11;
+    PartOnep4444.innerHTML = arr1[4].partOnep11;
+    PartOnep3333.innerHTML = arr1[4].partOnep11;
+    PartOnep2222.innerHTML = arr1[4].partOnep11;
+    PartOnep21.innerHTML = arr1[5].partOnep21;
+    PartOnep44444.innerHTML = arr1[5].partOnep21;
+    PartOnep33333.innerHTML = arr1[5].partOnep21;
+    PartOnep2122.innerHTML = arr1[5].partOnep21;
+    accept1.innerHTML  = arr1[6].Accept1 ;
+    accept1.style.backgroundColor = "#007580" ;
+    accept1.style.color = "#FAFAFA" ;
+    accept1.style.borderRadius = "15px" ;
+    accept1.style.width = "80px" ;
+    accept1.style.padding = "4px" ;
+    accept1.style.border = "none" ;
+    Closed1.innerHTML  = arr1[19].closed ; 
+    Closed1.style.backgroundColor = "#f15f66" ;
+    Closed1.style.color = "#FAFAFA" ;
+    Closed1.style.borderRadius = "15px" ;
+    Closed1.style.width = "80px" ;
+    Closed1.style.padding = "4px" ;
+    Closed1.style.border = "none" ;
+    reject1.innerHTML  = arr1[7].Reject1 ;
+    reject1.style.backgroundColor = "#E01B24" ;
+    reject1.style.color = "#FAFAFA" ;
+    reject1.style.borderRadius = "15px" ;
+    reject1.style.width = "80px" ;
+    reject1.style.padding = "4px" ;
+    reject1.style.border = "none" ;
+    Cicletwo.innerHTML = `<i class="fa-solid fa-circle"></i>` ;
+    Cicletwo4444.innerHTML = `<i class="fa-solid fa-circle"></i>` ;
+    Cicletwo3333.innerHTML = `<i class="fa-solid fa-circle"></i>` ;
+    Cicletwo2222.innerHTML = `<i class="fa-solid fa-circle"></i>` ;
+    PartOnep12.innerHTML  = arr1[8].partOnep12 ;
+    PartOnep124444.innerHTML  = arr1[8].partOnep12 ;
+    PartOnep123333.innerHTML  = arr1[8].partOnep12 ;
+    PartOnep122222.innerHTML  = arr1[8].partOnep12 ;
+    PartOnep22.innerHTML  = arr1[9].partOnep22 ;
+    PartOnep224444.innerHTML  = arr1[9].partOnep22 ;
+    PartOnep223333.innerHTML  = arr1[9].partOnep22 ;
+    PartOnep222222.innerHTML  = arr1[9].partOnep22 ;
+    accept2.innerHTML  = arr1[10].Accept2 ;
+    accept2.style.backgroundColor = "#007580" ;
+    accept2.style.color = "#FAFAFA" ;
+    accept2.style.borderRadius = "15px" ;
+    accept2.style.width = "80px" ;
+    accept2.style.padding = "4px" ;
+    accept2.style.border = "none" ;
+    Closed2.innerHTML  = arr1[19].closed ;
+    Closed2.style.backgroundColor = "#f15f66" ;
+    Closed2.style.color = "#FAFAFA" ;
+    Closed2.style.borderRadius = "15px" ;
+    Closed2.style.width = "80px" ;
+    Closed2.style.padding = "4px" ;
+    Closed2.style.border = "none" ;
+    reject2.innerHTML  = arr1[11].Reject2 ;
+    reject2.style.backgroundColor = "#E01B24" ;
+    reject2.style.color = "#FAFAFA" ;
+    reject2.style.borderRadius = "15px" ;
+    reject2.style.width = "80px" ;
+    reject2.style.padding = "4px" ;
+    reject2.style.border = "none" ;
+    Ciclethree.innerHTML  = `<i class="fa-solid fa-circle"></i>` ;
+    Ciclethree4444.innerHTML  = `<i class="fa-solid fa-circle"></i>` ;
+    Ciclethree3333.innerHTML  = `<i class="fa-solid fa-circle"></i>` ;
+    Ciclethree2222.innerHTML  = `<i class="fa-solid fa-circle"></i>` ;
+    PartOnep13.innerHTML = arr1[12].partOnep13 ;
+    PartOnep134444.innerHTML = arr1[12].partOnep13 ;
+    PartOnep133333.innerHTML = arr1[12].partOnep13 ;
+    PartOnep132222.innerHTML = arr1[12].partOnep13 ;
+    PartOnep23.innerHTML = arr1[13].partOnep23 ;
+    PartOnep234444.innerHTML = arr1[13].partOnep23 ;
+    PartOnep233333.innerHTML = arr1[13].partOnep23 ;
+    PartOnep232222.innerHTML = arr1[13].partOnep23 ;
+    accept3.innerHTML = arr1[14].Accept3 ;
+    accept3.style.backgroundColor = "#007580" ;
+    accept3.style.color = "#FAFAFA" ;
+    accept3.style.borderRadius = "15px" ;
+    accept3.style.width = "80px" ;
+    accept3.style.padding = "4px" ;
+    accept3.style.border = "none" ;
+    Closed3.innerHTML = arr1[19].closed ;
+    Closed3.style.backgroundColor = "#f15f66" ;
+    Closed3.style.color = "#FAFAFA" ;
+    Closed3.style.borderRadius = "15px" ;
+    Closed3.style.width = "80px" ;
+    Closed3.style.padding = "4px" ;
+    Closed3.style.border = "none" ;
+    
+    reject3.innerHTML = arr1[15].Reject3 ; 
+    reject3.style.backgroundColor = "#E01B24" ;
+    reject3.style.color = "#FAFAFA" ;
+    reject3.style.borderRadius = "15px" ;
+    reject3.style.width = "80px" ;
+    reject3.style.padding = "4px" ;
+    reject3.style.border = "none" ;
+    Divsps22.innerHTML = arr1[16].divsps2 ;
+    Divsps23333.innerHTML = arr1[17].divsps2 ;
+    Divsps24444.innerHTML = arr1[18].divsps2 ;
+
+}
+
+
+accept1.addEventListener("click" , code1)
+function code1(){
+    requestOneFirst.style.display = "none"
+    requestThreeFirst.style.display = "block"
+}
+
+
+
+accept2.addEventListener("click" , code2)
+function code2(){
+    requestOneSecond.style.display = "none"
+    requestThreeSecond.style.display = "block"
+}
+
+
+
+accept3.addEventListener("click" , code3)
+function code3(){
+    requestOnethird.style.display = "none"
+    requestThreeThird.style.display = "block"
+}
+
+// ///////////////////////////
+
+Closed1.addEventListener("click" , code4)
+function code4(){
+    requestThreeFirst.style.display = "none"
+    requestTwoFirst.style.display = "block"
+}
+
+
+Closed2.addEventListener("click" , code5)
+function code5(){
+    requestThreeSecond.style.display = "none"
+    RequestTwoSecond.style.display = "block"
+}
+
+Closed3.addEventListener("click" , code6)
+function code6(){
+    requestThreeThird.style.display = "none"
+    requestTwoThird.style.display = "block"
+}
+
+// /////////////////////////////////////
+
+reject1.addEventListener("click" , code7)
+function code7(){
+    requestOneFirst.style.display = "none"
+    RequestFourFirst.style.display = "block"
+}
+
+reject2.addEventListener("click" , code8)
+function code8(){
+    requestOneSecond.style.display = "none"
+    RequestFourSecond.style.display = "block"
+}
+
+
+reject3.addEventListener("click" , code9)
+function code9(){
+    requestOnethird.style.display = "none"
+    RequestFourThird.style.display = "block"
+}
